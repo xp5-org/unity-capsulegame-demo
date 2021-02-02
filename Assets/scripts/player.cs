8e1f4b358d72c0876d96477feeb434e9e3828a91
xp5-org <kylematthewholst@gmail.com>
2021-02-01 16:48:46 -0800

    the game

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 3, ForceMode.VelocityChange);
+
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
