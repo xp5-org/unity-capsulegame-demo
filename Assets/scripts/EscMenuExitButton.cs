a8edfbe592b75f1d488842dda4f9a63b48978ac5
xp5-org <kylematthewholst@gmail.com>
2021-02-09 22:03:05 -0800

    merged in menu button scripts
    json test on optons button, exit button now working

---
+++ Assets/scripts/EscMenuExitButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscMenuExitButton : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        Debug.Log("Esc-Menu exit Button clicked ");
+        Application.Quit();
+    }
+}
---
+++ Assets/scripts/EscMenuOptionsButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+using System.Net;
+using System;
+using System.IO;
+
+public class EscMenuOptionsButton : MonoBehaviour
+{
+    int n;
+    public void OnButtonPress()
+    {
+        n++;
+        Debug.Log("Esc-Menu Options Button clicked " + n + " times.");
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("https://reqbin.com/echo/get/json"));
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonResponse = reader.ReadToEnd();
+        Debug.Log(jsonResponse);
+
+        Transform[] allChildren = GetComponentsInChildren<Transform>();
+        foreach (Transform child in allChildren)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+    }
+}
